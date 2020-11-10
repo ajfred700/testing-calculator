1e958dbfb14f8ff68f75a8b2b51ac17ae6ca7a6b
ajfred700 <jose70020@gmail.com>
2020-11-09 22:19:02 -0600

    subs #2

--- src/app/substraction/substraction.spec.ts
+++ src/app/substraction/substraction.spec.ts
@@ -1,50 +1,26 @@
-import { substraction } from "./substraction"
 
-describe('substraction unit tests', () => {
-    
-    
-it('Should subs 0 -2  = -2', () => {
-
-// Pattern AAA (Arrange - Act - Assert)
-
-// Arrange
-let result = 0;
-
-// Act
-result = substraction(0, 2);
-
-// Assert
-expect(result).toBe(-2);
-})
-
-xit('Should subs 9 -3 -2 = 6', () => {
-// Arrange
-let result = 0;
-// Act
-result = substraction(0, 2);
-// Assert
-expect(result).toBe(-2);
-})
-xit('Should subs 3.1416 - 0.1416 = 3.0', () => {
-// Arrange
-let result = 0;
-// Act
-result = substraction(3.1416, 0.1416);
-// Assert
-expect(result).toBe(3.0);
-})
-xit('Should subs 2.7 - 0.7 = 2.0', () => {
-    // Arrange
-let result = 0;
-// Act
-result = substraction(2.0, 0.7);
-// Assert
-expect(result).toBe(2.0);
-})
-
-}) 
-
-
-
-
-
+import {from} from 'rxjs';
+import { substraction } from './substraction';
+describe('substraction unit tests',() => {
+  //AAA
+  it('should 2 - 2 = 0',()=>{
+    let result = 0;
+    result = substraction(2,2);
+    expect(result).toBe(0);
+  })
+  it(' should 0 - -2 = 2',()=>{
+    let result = 0;
+    result = substraction(0,-2);
+    expect(result).toBe(2);
+  })
+  it('should 3.0 - 0.1416 = 2.8584',()=>{
+    let result = 0;
+    result = substraction(3.0,.1416);
+    expect(result).toBe(2.8584);
+  })
+  it('should 2.0 - 0.7 = 1.3',()=>{
+    let result = 0;
+    result = substraction(2.0,0.7);
+    expect(result).toBe(1.3);
+  })
+});
